7c915eded1db45602a8c75160de049a333b9d983
korovindenis <korovindenis@yandex.ru>
2021-03-11 18:33:44 +0300

    js

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,6 +1,12 @@
 const newPass = document.getElementById("newPass");
 const resPass = document.getElementById("resPass");
+const quantity = document.getElementById("quantity");
 
+quantity.onchange = function() {
+    if (isNaN(this.value) || this.value < 1 || this.value > 12) {
+        this.value = 1;
+    }
+}
 newPass.onclick = function() {
-    resPass.value = Math.random().toString(36).slice(-8);
-};
+    resPass.value = Math.random().toString(36).slice(-quantity.value);
+}
